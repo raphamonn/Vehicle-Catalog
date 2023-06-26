b337699b68750055dd97ccb8e26dd51eac9fc7f7
Rafael Passos <rafaelmpassos01@gmail.com>
2023-06-26 10:52:48 -0300

    added urls inside app

--- project/urls.py
+++ project/urls.py
@@ -15,15 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-from recipes.views import home, contato, sobre
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('contato/', contato),
-    path('sobre/', sobre),
+    path('', include('recipes.urls')),  # /
+
 
 ]
