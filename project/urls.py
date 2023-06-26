d1d9d7c1b7fc0caa19a32c1ac528b1d4de8df937
Rafael Passos <rafaelmpassos01@gmail.com>
2023-06-26 10:38:05 -0300

    added new app

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+from recipes.views import home, contato, sobre
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+    path('contato/', contato),
+    path('sobre/', sobre),
+
 ]
