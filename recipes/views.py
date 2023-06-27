2a44ec1e3f156653afcd99732838d6fff47d941d
Rafael Passos <rafaelmpassos01@gmail.com>
2023-06-27 14:22:35 -0300

    Removed "contato" and "sobre" views

--- recipes/views.py
+++ recipes/views.py
@@ -1,18 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
     return render(request, 'recipes/home.html',
                   context={'name': 'Rafael Passos'},
                   )
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html',
-                  context={'name': 'Rafael Passos'}
-                  )
-
-
-def sobre(request):
-    return HttpResponse('sobre')
