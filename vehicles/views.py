e7a7df59d6142d8d1760d970979e958c633676fc
Rafael Passos <rafaelmpassos01@gmail.com>
2023-07-06 14:36:17 -0300

    changed category title

--- vehicles/views.py
+++ vehicles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from utils.vehicles.factory import make_vehicle
 from vehicles.models import Vehicle
 
@@ -13,17 +13,26 @@ def home(request):
 
 
 def category(request, category_id):
-    vehicles = Vehicle.objects.filter(
-        category__id=category_id,
-        is_published=True
-    ).order_by('-id')
+    vehicles = get_list_or_404(
+        Vehicle.objects.filter(
+            category__id=category_id,
+            is_published=True
+        ).order_by('-id'))
+
     return render(request, 'vehicles/pages/category.html', context={
-        'vehicles': vehicles
+        'vehicles': vehicles,
+        'title': f'{vehicles[0].category.name}'
     })
 
 
 def vehicle(request, id):
+
+    vehicle = Vehicle.objects.filter(
+        pk=id,
+        is_published=True
+    ).order_by('-id').first()
+
     return render(request, 'vehicles/pages/vehicle_view.html', context={
-        'vehicle': make_vehicle(),
+        'vehicle': vehicle,
         'is_detail_page': True,
     })
