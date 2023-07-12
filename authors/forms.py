4874e50701d0ff980f5d476eae768ab2839dfca3
Rafael Passos <rafaelmpassos01@gmail.com>
2023-07-12 14:06:24 -0300

    added register_view html and forms

--- authors/forms.py
+++ authors/forms.py
@@ -1,14 +1,52 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.forms import widgets
 
 
 class RegisterForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['username',
-                  'first_name',
-                  'last_name',
-                  'email',
-                  'password',
 
-                  ]
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Type your Password here'
+        }),
+        label='Senha',
+        help_text=(
+            'Password must have at least one uppercase letter,'
+            'one lowercase letter and one number. The length should be'
+            'at least 8 characters'),
+        error_messages={
+            'required': 'Password must not be empty'
+        }
+
+    )
+
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repeat your Password here'
+        }),
+        label='Confirmação de senha'
+    )
+
+    class Meta:
+
+        model = User
+        fields = [
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password',
+        ]
+        error_messages = {
+            'username': {
+                'required': 'This Field must not be empty',
+            }
+        }
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'placeholder': 'Type your username here'
+
+            }),
+        }
